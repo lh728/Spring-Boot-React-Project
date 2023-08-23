b585fac07fbefcf01369436c55ee1523274b88b5
lh728 <425658167@qq.com>
2023-08-23 21:53:10 +0800

    unfetch2

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import {getAllStudents} from './client';
 
 function App() {
-  <h1> hello world! </h1>
-  return (
+  state = {
+    student: []
+  }
+  fetchStudents = () => {
     getAllStudents().then(res => res.json().then(students => {
       console.log(students);
     }))
+  }
+  return (
+    <h1> hello world! </h1>
+
   );
 }
 
