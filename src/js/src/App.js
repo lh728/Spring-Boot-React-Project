e02b7d8565aaedac061456e19395430e804c5113
hongjin <425658167@qq.com>
2023-11-02 22:09:24 +0800

    add getAllStudents method

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {getAllStudents} from './client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+
+  render(){ 
+
+    getAllStudents().then((res) => {
+      res.json().then(students => {
+        console.log(students);
+      })
+    });
+
+    return (
+      <div className="App">
+        <h1> luhongjin</h1>
+      </div>
+    );
+  }
+
 }
 
 export default App;
