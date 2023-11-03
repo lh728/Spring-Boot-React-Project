deb80db6dbdd18319f4d174080b0a7dace4c124f
hongjin <425658167@qq.com>
2023-11-03 22:55:23 +0800

    update template

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -4,17 +4,48 @@ import {getAllStudents} from './client';
 
 class App extends Component  {
 
-  render(){ 
+  state = {
+    students: []
+  }
 
+  componentDidMount(){
+    this.fetchStudents();
+  }
+
+  fetchStudents = () => {
     getAllStudents().then((res) => {
       res.json().then(students => {
-        console.log(students);
+        this.setState({
+          students
+        });
       })
     });
+  }
+
+
+  render(){ 
+    
+    const {students} = this.state;
+
+    if(students && students.length){
+      return students.map((student,index) => {
+        return (
+          <div key={index}>
+            <h2>{student.studentId}</h2>
+            <h2>{student.firstName}</h2>
+            <h2>{student.lastName}</h2>
+            <h2>{student.gender}</h2>
+            <h2>{student.email}</h2>
+          </div>
+        );
+      })
+
+    }
+
 
     return (
       <div className="App">
-        <h1> luhongjin</h1>
+        <h1> No Students found</h1>
       </div>
     );
   }
