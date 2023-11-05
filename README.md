1875837144aa1559015d05dbee9fc4bb5beea5dc
hongjin <425658167@qq.com>
2023-11-05 20:28:22 +0800

    import and use ant design

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Spring-Boot-React-Project
 SpringBoot + React + Maven + Docker + Spring Data + AWS RDS + Beanstalk + Git + Slack + Unit Testing
+
+
+
+### Version Description
+
+- Spring Boot 3.1.2
+- Maven 3.9.2
+- Node.js v20.9
+- React 18.2.0
+- Ant Design 5.11.0
--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import {getAllStudents} from './client';
+import {
+  Table
+} from 'antd';
 
 class App extends Component  {
 
@@ -27,18 +30,36 @@ class App extends Component  {
     
     const {students} = this.state;
 
-    if(students && students.length){
-      return students.map((student,index) => {
-        return (
-          <div key={index}>
-            <h2>{student.studentId}</h2>
-            <h2>{student.firstName}</h2>
-            <h2>{student.lastName}</h2>
-            <h2>{student.gender}</h2>
-            <h2>{student.email}</h2>
-          </div>
-        );
-      })
+    if(students && students.length){   
+      const columns = [
+        {
+          title:'StudentId',
+          dataIndex:'studentId',
+          key:'studentId'
+        },
+        {
+          title:'First Name',
+          dataIndex:'firstName',
+          key:'firstName'
+        },
+        {
+          title:'Last Name',
+          dataIndex:'lastName',
+          key:'lastName'
+        },
+        {
+          title:'Email',
+          dataIndex:'email',
+          key:'email'
+        },
+        {
+          title:'Gender',
+          dataIndex:'gender',
+          key:'gender'
+        },
+      ];
+      
+      return (<Table dataSource={students} columns={columns} rowKey='studentId' />);
 
     }
 
