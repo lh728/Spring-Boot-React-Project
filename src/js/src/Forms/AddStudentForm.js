d7c5cbf1a9dacf2d9b8ba7e811fae235574cfbb4
hongjin <425658167@qq.com>
2023-11-30 19:47:39 +0800

    form  validation

--- src/js/src/Forms/AddStudentForm.js
+++ src/js/src/Forms/AddStudentForm.js
@@ -11,12 +11,21 @@ class AddStudentForm extends Component {
                 initialValues={{ firstName: '', lastName: '' , email: '', gender: ''}}
                 validate={values => {
                     const errors = {};
+                    if (!values.firstName) {
+                        errors.firstName = 'Required';
+                    }
+                    if (!values.lastName) {
+                        errors.lastName = 'Required';
+                    }
                     if (!values.email) {
                         errors.email = 'Required';
                     } else if (
                         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                     ) {
                         errors.email = 'Invalid email address';
+                    }
+                    if (!values.gender) {
+                        errors.gender = 'Required';
                     }
                     return errors;
                 }}
