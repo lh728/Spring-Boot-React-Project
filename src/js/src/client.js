464c59d129e8eb0dfea27ea751b229184f8b5c99
hongjin <425658167@qq.com>
2023-12-04 21:50:24 +0800

    client side post

--- src/js/src/client.js
+++ src/js/src/client.js
@@ -1,3 +1,11 @@
 import fetch from 'unfetch';
 
 export const getAllStudents = () => fetch('api/students');
+
+export const addNewStudent = studnet => fetch('api/students',{
+    headers:{
+        'Content-Type':'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(studnet)
+});
