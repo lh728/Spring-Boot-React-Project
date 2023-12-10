48c39f1e854d0ff9a7f0c6690bc79db25d385aa6
hongjin <425658167@qq.com>
2023-12-10 16:48:45 +0800

    handle error

--- src/js/src/Forms/AddStudentForm.js
+++ src/js/src/Forms/AddStudentForm.js
@@ -7,95 +7,96 @@ const inputButtonMargin = { marginBottom: '10px' };
 const tagStyle = { backgroundColor: '#f50', color: 'white', ...inputButtonMargin };
 
 const AddStudentForm = (props) => {
-
-    <Formik
-        initialValues={{ firstName: '', lastName: '', email: '', gender: '' }}
-        validate={values => {
-            const errors = {};
-            if (!values.firstName) {
-                errors.firstName = 'Required';
-            }
-            if (!values.lastName) {
-                errors.lastName = 'Required';
-            }
-            if (!values.email) {
-                errors.email = 'Required';
-            } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-                errors.email = 'Invalid email address';
-            }
-            if (!values.gender) {
-                errors.gender = 'Required';
-            }
-            return errors;
-        }}
-        onSubmit={(student, { setSubmitting }) => {
-            addNewStudent(student).then(() => {
-                props.onSuccess();
-                setSubmitting(false);
-            })
-        }}
-    >
-        {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            submitForm,
-            isValid
-            /* and other goodies */
-        }) => (
-            <form onSubmit={handleSubmit}>
-                <Input
-                    name="firstName"
-                    style={inputButtonMargin}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.firstName}
-                    placeholder="First Name. E.g John"
-                />
-                {errors.firstName && touched.firstName && <Tag style={tagStyle}>{errors.firstName}</Tag>}
-                <Input
-                    name="lastName"
-                    style={inputButtonMargin}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.lastName}
-                    placeholder="Last Name. E.g Smith"
-                />
-                {errors.lastName && touched.lastName && <Tag style={tagStyle}>{errors.lastName}</Tag>}
-                <Input
-                    type="email"
-                    name="email"
-                    style={inputButtonMargin}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    placeholder="Email E.g example@gamil.com "
-                />
-                {errors.email && touched.email && <Tag style={tagStyle}>{errors.email}</Tag>}
-                <Input
-                    name="gender"
-                    style={inputButtonMargin}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.gender}
-                    placeholder="Gender E.g Male or Female"
-                />
-                {errors.gender && touched.gender && <Tag style={tagStyle}>{errors.gender}</Tag>}
-                <Button
-                    onClick={() => submitForm()}
-                    type="submit"
-                    disabled={isSubmitting | (touched && !isValid)}>
-                    Submit
-                </Button>
-            </form>
-        )}
-    </Formik>
+    return (
+        <Formik
+            initialValues={{ firstName: '', lastName: '', email: '', gender: '' }}
+            validate={values => {
+                const errors = {};
+                if (!values.firstName) {
+                    errors.firstName = 'Required';
+                }
+                if (!values.lastName) {
+                    errors.lastName = 'Required';
+                }
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                    errors.email = 'Invalid email address';
+                }
+                if (!values.gender) {
+                    errors.gender = 'Required';
+                }
+                return errors;
+            }}
+            onSubmit={(student, { setSubmitting }) => {
+                addNewStudent(student).then(() => {
+                    props.onSuccess();
+                    setSubmitting(false);
+                })
+            }}
+        >
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                submitForm,
+                isValid
+                /* and other goodies */
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <Input
+                        name="firstName"
+                        style={inputButtonMargin}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.firstName}
+                        placeholder="First Name. E.g John"
+                    />
+                    {errors.firstName && touched.firstName && <Tag style={tagStyle}>{errors.firstName}</Tag>}
+                    <Input
+                        name="lastName"
+                        style={inputButtonMargin}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.lastName}
+                        placeholder="Last Name. E.g Smith"
+                    />
+                    {errors.lastName && touched.lastName && <Tag style={tagStyle}>{errors.lastName}</Tag>}
+                    <Input
+                        type="email"
+                        name="email"
+                        style={inputButtonMargin}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                        placeholder="Email E.g example@gamil.com "
+                    />
+                    {errors.email && touched.email && <Tag style={tagStyle}>{errors.email}</Tag>}
+                    <Input
+                        name="gender"
+                        style={inputButtonMargin}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.gender}
+                        placeholder="Gender E.g Male or Female"
+                    />
+                    {errors.gender && touched.gender && <Tag style={tagStyle}>{errors.gender}</Tag>}
+                    <Button
+                        onClick={() => submitForm()}
+                        type="submit"
+                        disabled={isSubmitting | (touched && !isValid)}>
+                        Submit
+                    </Button>
+                </form>
+            )}
+        </Formik>
+    )
 
 
 }
--- src/js/src/client.js
+++ src/js/src/client.js
@@ -1,11 +1,24 @@
 import fetch from 'unfetch';
 
-export const getAllStudents = () => fetch('api/students');
+export const getAllStudents = () => fetch('api/students').then(checkStatus);
 
-export const addNewStudent = studnet => fetch('api/students',{
-    headers:{
-        'Content-Type':'application/json'
+export const checkStatus = (response) => {
+    if (response.ok) {
+        return response;
+    } else {
+        let error = new Error(response.statusText)
+        error.response = response;
+        response.json().then(e => {
+            error.error = e;
+        });
+        return Promise.reject(error);
+    }
+}
+
+export const addNewStudent = studnet => fetch('api/students', {
+    headers: {
+        'Content-Type': 'application/json'
     },
     method: 'POST',
     body: JSON.stringify(studnet)
-});
+}).then(checkStatus);
